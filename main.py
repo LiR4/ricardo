2164a4a37e4e6f3c67bd76726c2893b7af457151
LiR4 <pedrolira1227@gmail.com>
2021-08-03 23:46:02 -0300

    added first

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#Our main file
+# Our main file
+
+import speech_recognition as sr
+
+# Cria um reconhecedor
+r = sr.Recognizer()
+
+# Abri o microfone para captura
+with sr.Microphone() as source:
+    audio = r.listen(source)
+    while True:
+        print(r.recognize_google(audio))
