bf18b4edcd6c27282aed164e0b652203c03054e4
LiR4 <83731045+LiR4@users.noreply.github.com>
2021-10-14 14:08:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ r = sr.Recognizer()
 
 # Abri o microfone para captura
 with sr.Microphone() as source:
-    audio = r.listen(source)
     while True:
+        audio = r.listen(source)
+        
         print(r.recognize_google(audio))
